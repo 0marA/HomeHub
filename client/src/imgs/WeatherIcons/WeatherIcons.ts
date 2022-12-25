7f3c90513bd6bbaa0fc85f9d082ca6063d1ee9cc
0marA <theengineermachine@gmail.com>
2022-12-25 00:14:01 -0600

    Add Images

---
+++ client/src/imgs/WeatherIcons/WeatherIcons.ts
@@ -0,0 +1,23 @@
+const IMAGES = {
+    am_pcloudy: require("./am_pcloudy.png"),
+    am_pcloudyr: require("./am_pcloudyr.png"),
+    am_showers: require("./am_showers.png"),
+    am_tstorm: require("./am_tstorm.png"),
+    clear: require("./clear.png"),
+    cloudy: require("./cloudy.png"),
+    fog: require("./fog.png"),
+    pcloudy: require("./pcloudy.png"),
+    pcloudyr: require("./pcloudyr.png"),
+    rain: require("./rain.png"),
+    sunny: require("./sunny.png"),
+    tstorm: require("./tstorm.png"),
+    wind: require("./wind.png"),
+    pm_pcloudy: require("./pm_pcloudy.png"),
+    fair: require("./fair.png"),
+    sunrise: require("./sunrise.png"),
+    sunset: require("./sunset.png"),
+    happynight: require("./happynight.png"),
+    halfmoon: require("./half-moon.png"),
+};
+
+export default IMAGES;
